df00bc1f8e4ad3a8b07e6087acf2b35df9b27d40
herivillaz <herivillaz@gmail.com>
2020-07-24 13:32:05 -0400

    delete comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,24 +12,9 @@ var notesData = require("../db/db.json");
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
   app.get("/api/notes", function(req, res) {
     res.json(notesData);
   });
-
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/notes", function(req, res) {
     console.log(req.body);

3648bfd82c7b05639a27301f110b6f0f8787b3f7
herivillaz <herivillaz@gmail.com>
2020-07-24 17:44:59 -0400

    fix bug, modify read me

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Note-Taker
 Application that can be used to write, save, and delete notes
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+* **The final and deployed page looks like this**
+
+![Home Page](/Assets/homepage.png)
+
+![Note Block](Assets/noteblock.png)
+
+### You can access here: [Home Page](public/index.html)
+
+* The following Gif/video demostrates the app Functionality
+
+* Adding note:
+
+![Manager](Assets/test.gif)
+
+This project was made taking in consideration the requirements from Columbia Coding Boot Camp:
+
+Create an application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.
+
+## Acceptance Criteria
+
+Application should allow users to create and save notes.
+
+Application should allow users to view previously saved notes.
+
+Application should allow users to delete previously saved notes.
+
+# Description
+
+In the Business Context For users that need to keep track of a lot of information, it's easy to forget or be unable to recall something important. Being able to take persistent notes allows users to have written information available when needed.
+
+# Usage
+In your commandline, while in the directory where your server.js file is, run "node server.js". Output will be generated in the directory output with name "team.html"
+
+# Contribution
+If you would like to contribute please create an issue. Alternatively, clone this repo and make modifications to your copy.
+
+# Test
+Remember, you can run the tests at any time with `npm run test`
+
+# Author
+* Heribert Villazana
+* Github: herivillaz
+
+![Repo Owner Image](https://avatars.githubusercontent.com/herivillaz?s=100)
+
+## Contact
+herivillaz@gmail.com
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,6 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-var notesData = require("../db/db.json");
 var fs = require('fs');
 var path = require('path');
 var uuid = require("uuid");
@@ -17,40 +13,36 @@ var dbPath = path.join(__dirname, "../db/db.json");
 // ===============================================================================
 
 module.exports = function (app) {
-  app.post("/api/notes", function (req, res) {
-  fs.readFile(dbPath, "utf8", function (err, data) {
-    if (err) {
-      throw err;
-    }
-    return res.json(data);
+  app.get("/api/notes", function (req, res) {
+    fs.readFile(dbPath, "utf8", function (err, data) {
+      if (err) {
+        throw err;
+      }
+      return res.json(JSON.parse(data));
+    })
   })
-})
-
-
 
   // app.post("/api/notes", function (req, res) {
   //   console.log(req.body);
   //   notesData.push(req.body);
   //   res.json(notesData);
-
-
   // });
   app.post("/api/notes", function(req, res) {
     fs.readFile(dbPath, "utf8", function (err1, data1) {
         if (err1) {
             throw err1;
         }
-        console.log(typeof(data1));
-        let dataArr = JSON.parse(data1);
-        let entry = req.body;
-        entry.id = uuid.v4();
-        dataArr.push(entry);
-        fs.writeFile(dbPath, JSON.stringify(dataArr, null, 2), function (err2) {
-            console.log("done writing");
+        // console.log(typeof(data1));
+        let arrData = JSON.parse(data1);
+        let note = req.body;
+        note.id = uuid.v4();
+        arrData.push(note);
+        fs.writeFile(dbPath, JSON.stringify(arrData, null, 2), function (err2) {
+            console.log("Note added");
             if (err2) {
                 throw err2;
             }
-            return res.json(dataArr);
+            return res.json(arrData);
         });
     })
 
@@ -75,7 +67,7 @@ app.delete("/api/notes/:id", function(req, res) {
   
       fs.writeFile(dbPath, JSON.stringify(noteObjRemain), function (err, data) {
           if (err) throw err;
-          console.log("file saved with obj removed");
+          console.log("obj removed");
           return res.json(noteObj);
       })
   })
